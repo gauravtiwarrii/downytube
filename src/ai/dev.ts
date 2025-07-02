2303d5b876af44e6a283a39481c7220421dfd35f
Gaurav Tiwari <igauravtiwari1096@gmail.com>
2025-07-02 05:34:40 +0000

    now this

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,3 @@
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/optimize-youtube-tags.ts';
 import '@/ai/flows/rewrite-video-details.ts';
 import '@/ai/flows/generate-thumbnail.ts';
